9b0ab15326bce8958ff7b817da64dc63dc667bb9
aetin0 <anlas@protonmail.com>
2017-01-24 21:47:30 +0100

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -1,1 +1,10 @@
 # notes
+
+## strace 
+strace process 2>&1 | grep word
+strace -e open process 2>&1 | grep file
+strace -e open,access 2>&1 | grep file
+strace -p pid
+strace -c -p pid
+strace -e poll,select,connect,recvfrom,sendto nc www.example.com 80
+
